2d4dcf48cc19ec0a9eb0edd1e2953dc835efdf62
mostafa_anter <ga30000@gmail.com>
2016-10-22 00:23:39 +0200

    stable version

--- app/src/main/java/com/perfect_apps/tawsili/activities/AskForVerificationCodeActivity.java
+++ app/src/main/java/com/perfect_apps/tawsili/activities/AskForVerificationCodeActivity.java
@@ -1,14 +1,14 @@
 package com.perfect_apps.tawsili.activities;
 
-import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.akexorcist.localizationactivity.LocalizationActivity;
@@ -17,52 +17,31 @@ import com.perfect_apps.tawsili.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SignUpActivity extends LocalizationActivity implements View.OnClickListener {
+public class AskForVerificationCodeActivity extends LocalizationActivity implements View.OnClickListener {
 
     @BindView(R.id.toolbar) Toolbar toolbar;
-    @BindView(R.id.text1) TextView textView1;
-    @BindView(R.id.text2) TextView textView2;
 
     @BindView(R.id.button1)
     Button button1;
 
-    @BindView(R.id.editText1) EditText editText1;
-    @BindView(R.id.editText2) EditText editText2;
-    @BindView(R.id.editText3) EditText editText3;
-    @BindView(R.id.editText4) EditText editText4;
-    @BindView(R.id.editText5) EditText editText5;
-    @BindView(R.id.editText6) EditText editText6;
-
-    @BindView(R.id.checkbox1)
-    CheckBox checkBox1;
-
-    @BindView(R.id.signUpUsingFacebook)
-    LinearLayout linearLayout1;
+    @BindView(R.id.editText1)
+    EditText editText1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_ask_for_verification_code);
+
         ButterKnife.bind(this);
         setToolbar();
         changeFontOfText();
-        linearLayout1.setOnClickListener(this);
         button1.setOnClickListener(this);
     }
 
     private void changeFontOfText(){
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/normal.ttf");
-        textView1.setTypeface(font);
-        textView2.setTypeface(font);
-
         button1.setTypeface(font);
         editText1.setTypeface(font);
-        editText2.setTypeface(font);
-        editText3.setTypeface(font);
-        editText4.setTypeface(font);
-        editText5.setTypeface(font);
-        editText6.setTypeface(font);
-        checkBox1.setTypeface(font);
 
     }
 
@@ -90,18 +69,8 @@ public class SignUpActivity extends LocalizationActivity implements View.OnClick
 
     }
 
-    public void onCheckboxClicked(View view) {
-    }
-
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.button1:
-                startActivity(new Intent(SignUpActivity.this, AskForVerificationCodeActivity.class));
-                break;
-            case R.id.signUpUsingFacebook:
-                startActivity(new Intent(SignUpActivity.this, AskForEmailActivity.class));
-                break;
-        }
+
     }
 }
