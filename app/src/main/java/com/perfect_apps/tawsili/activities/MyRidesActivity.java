49345e81adec770f5252f71be82aee5cab8080f2
mostafa_anter <ga30000@gmail.com>
2016-10-23 10:44:34 +0200

    stable version

--- app/src/main/java/com/perfect_apps/tawsili/activities/MyRidesActivity.java
+++ app/src/main/java/com/perfect_apps/tawsili/activities/MyRidesActivity.java
@@ -2,9 +2,16 @@ package com.perfect_apps.tawsili.activities;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,6 +24,12 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.perfect_apps.tawsili.R;
+import com.perfect_apps.tawsili.adapters.MyRidesItemsAdapter;
+import com.perfect_apps.tawsili.models.MyRidesItem;
+import com.perfect_apps.tawsili.utils.DividerItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,6 +37,26 @@ import butterknife.ButterKnife;
 public class MyRidesActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.swipeRefresh)SwipeRefreshLayout mSwipeRefresh;
+
+    private static final String TAG = "ProviderChatsFragment";
+    private static final String KEY_LAYOUT_MANAGER = "layoutManager";
+    private static final int SPAN_COUNT = 2;
+
+
+    protected LayoutManagerType mCurrentLayoutManagerType;
+
+    private enum LayoutManagerType {
+        GRID_LAYOUT_MANAGER,
+        LINEAR_LAYOUT_MANAGER
+    }
+
+    protected MyRidesItemsAdapter mAdapter;
+    protected RecyclerView.LayoutManager mLayoutManager;
+    protected List<MyRidesItem> mDataset;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +74,55 @@ public class MyRidesActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void setmRecyclerView(Bundle savedInstanceState){
+        // initiate mDataSet
+        mDataset = new ArrayList<>();
+
+        mLayoutManager = new LinearLayoutManager(this);
+
+        mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+
+        if (savedInstanceState != null) {
+            // Restore saved layout manager type.
+            mCurrentLayoutManagerType = (LayoutManagerType) savedInstanceState
+                    .getSerializable(KEY_LAYOUT_MANAGER);
+        }
+        setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
+
+        mAdapter = new MyRidesItemsAdapter(this, mDataset);
+        // Set CustomAdapter as the adapter for RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+
+        RecyclerView.ItemDecoration itemDecoration = new
+                DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST);
+        mRecyclerView.addItemDecoration(itemDecoration);
+
+
+        //noinspection ResourceAsColor
+        mSwipeRefresh.setColorScheme(
+                R.color.swipe_color_1, R.color.swipe_color_2,
+                R.color.swipe_color_3, R.color.swipe_color_4);
+        mSwipeRefresh.setProgressViewOffset(false, 0,
+                (int) TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        24,
+                        getResources().getDisplayMetrics()));
+
+
+        mSwipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.i(TAG, "onRefresh called from SwipeRefreshLayout");
+
+                initiateRefresh();
+            }
+        });
+
+        initiateRefresh();
+        if (mSwipeRefresh != null &&!mSwipeRefresh.isRefreshing())
+            mSwipeRefresh.setRefreshing(true);
     }
 
     private void setToolbar() {
@@ -88,5 +170,67 @@ public class MyRidesActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void setRecyclerViewLayoutManager(LayoutManagerType layoutManagerType) {
+        int scrollPosition = 0;
+
+        // If a layout manager has already been set, get current scroll position.
+        if (mRecyclerView.getLayoutManager() != null) {
+            scrollPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }
+
+        switch (layoutManagerType) {
+            case GRID_LAYOUT_MANAGER:
+                mLayoutManager = new GridLayoutManager(this, SPAN_COUNT);
+                mCurrentLayoutManagerType =  LayoutManagerType.GRID_LAYOUT_MANAGER;
+                break;
+            case LINEAR_LAYOUT_MANAGER:
+                mLayoutManager = new LinearLayoutManager(this);
+                mCurrentLayoutManagerType =  LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+                break;
+            default:
+                mLayoutManager = new LinearLayoutManager(this);
+                mCurrentLayoutManagerType =  LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+        }
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.scrollToPosition(scrollPosition);
+    }
+
+    private void initiateRefresh(){
+        new  FakTask().execute();
+    }
+
+    private void onRefreshComplete(){
+        if (mSwipeRefresh.isRefreshing()) {
+            mSwipeRefresh.setRefreshing(false);
+        }
+    }
+
+    private class FakTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            MyRidesItem inboxItem = new MyRidesItem();
+            for (int i = 0;  i < 20; i++){
+                mDataset.add(inboxItem);
+                mAdapter.notifyDataSetChanged();
+            }
+
+            onRefreshComplete();
+        }
     }
 }
---
+++ app/src/main/java/com/perfect_apps/tawsili/models/MyRidesItem.java
@@ -0,0 +1,11 @@
+package com.perfect_apps.tawsili.models;
+
+/**
+ * Created by mostafa_anter on 10/23/16.
+ */
+
+public class MyRidesItem {
+    public MyRidesItem(){
+
+    }
+}
