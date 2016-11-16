67a3b43fb9fd0e6e5c6e0dc70e2088aa512b65c0
mostafa_anter <ga30000@gmail.com>
2016-11-16 17:22:16 +0200

    stable version

--- app/src/main/java/com/perfect_apps/tawsili/activities/ScheduleTesultActivity.java
+++ app/src/main/java/com/perfect_apps/tawsili/activities/ScheduleTesultActivity.java
@@ -1,26 +1,30 @@
 package com.perfect_apps.tawsili.activities;
 
-import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.akexorcist.localizationactivity.LocalizationActivity;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.perfect_apps.tawsili.R;
+import com.perfect_apps.tawsili.app.AppController;
+import com.perfect_apps.tawsili.models.DriverDurationAndDistance;
 import com.perfect_apps.tawsili.models.SchedualObject;
-import com.perfect_apps.tawsili.scheduleing_task.PushLocalNotification;
+import com.perfect_apps.tawsili.parser.JsonParser;
 import com.perfect_apps.tawsili.store.SceduleStore;
 import com.perfect_apps.tawsili.store.TawsiliPrefStore;
 import com.perfect_apps.tawsili.utils.Constants;
 import com.perfect_apps.tawsili.utils.OrderDriver;
-import com.perfect_apps.tawsili.utils.SweetDialogHelper;
-import com.perfect_apps.tawsili.utils.Utils;
+import com.perfect_apps.tawsili.utils.TawsiliPublicFunc;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,6 +34,9 @@ public class ScheduleTesultActivity extends LocalizationActivity implements View
     Toolbar toolbar;
     @BindView(R.id.button1)Button b1;
     @BindView(R.id.button2)Button b2;
+
+    private String mEstimateFare = "0";
+    private String distanceWithKilo = "0";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -92,28 +99,99 @@ public class ScheduleTesultActivity extends LocalizationActivity implements View
     }
 
    private void createOrder(){
-//        SchedualObject schedualObject = new SceduleStore(this).findItem(getIntent().getStringExtra("ID"));
-//
-//
-//        if (schedualObject != null) {
-//            String mCategoryValue;
-//            String mCategoryName;
-//            String currentLocationInfo;
-//            String dropOffLocationInfo;
-//            String mEstimateFare;
-//            String tripTime;
-//            String distanceWithKilo;
-//            String promoCode;
-//            String la;
-//            String lng;
-//            // set pref of last loc and  drop off loc
-//
-//
-//            if (!lat.trim().isEmpty() && !lng.trim().isEmpty()) {
-//                new OrderDriver(this, mCategoryValue, mCategoryName, currentLocationInfo
-//                        , dropOffLocationInfo, mEstimateFare, tripTime,
-//                        "2", distanceWithKilo, mCategoryValue, promoCode, "Later");
-//            }
-//        }
+        SchedualObject schedualObject = new SceduleStore(this).findItem(getIntent().getStringExtra("ID"));
+        if (schedualObject != null) {
+            String mCategoryValue = schedualObject.getOrder_category();
+            String mCategoryName;
+            switch (mCategoryValue) {
+                case "1":
+                    mCategoryName = "Economy";
+                    break;
+                case "2":
+                    mCategoryName = "Business";
+                    break;
+                case "3":
+                    mCategoryName = "Vip";
+                    break;
+                case "4":
+                    mCategoryName = "Family_Regular";
+                    break;
+                case "5":
+                    mCategoryName = "Family_Special";
+                    break;
+                default:
+                    mCategoryName = "Economy";
+                    break;
+            }
+            String tripTime = schedualObject.getOrder_start_time();
+            String currentLocationInfo = schedualObject.getFrom_details();
+            String dropOffLocationInfo = schedualObject.getTo_details();
+            String promoCode = schedualObject.getPromocode();
+
+            String lat = schedualObject.getFrom_location_lat();
+            String lng = schedualObject.getFrom_location_lng();
+
+            String latDrop = schedualObject.getTo_location_lat();
+            String lngDrop = schedualObject.getTo_location_lng();
+
+
+            new TawsiliPrefStore(this).addPreference(Constants.userLastLocationLat, lat);
+            new TawsiliPrefStore(this).addPreference(Constants.userLastLocationLng, lng);
+
+            new TawsiliPrefStore(this).addPreference(Constants.userLastDropOffLocationLat, latDrop);
+            new TawsiliPrefStore(this).addPreference(Constants.userLastDropOffLocationLng, lngDrop);
+
+            try {
+                if (Double.valueOf(latDrop) > 0
+                        && Double.valueOf(lngDrop) > 0
+                        && distanceWithKilo.equalsIgnoreCase("0")
+                        && mEstimateFare.equalsIgnoreCase("0")){
+                    getDriverDurationFromUserAndCalculateFare(lat, lng, latDrop, lngDrop, mCategoryValue);
+                    return;
+                }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+
+            if (!lat.trim().isEmpty() && !lng.trim().isEmpty()) {
+                new OrderDriver(this, mCategoryValue, mCategoryName, currentLocationInfo
+                        , dropOffLocationInfo, mEstimateFare, tripTime,
+                        "2", distanceWithKilo, mCategoryValue, promoCode, "Later");
+            }
+        }
     }
+
+    private void getDriverDurationFromUserAndCalculateFare(String userLat, String userLng,
+                                                           String driverLat, String driverLng,
+                                                           final String mCategoryValue) {
+        String url = TawsiliPublicFunc.createMatrixUri(userLat, userLng, driverLat, driverLng);
+        StringRequest strReq = new StringRequest(Request.Method.GET,
+                url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d("duration", response.toString());
+                List<DriverDurationAndDistance> driverDurationAndDistanceList =
+                        JsonParser.parseDistanceAndDuration(response);
+
+                if (driverDurationAndDistanceList != null && driverDurationAndDistanceList.size() > 0) {
+                    DriverDurationAndDistance mDriverDurationAndDistance = driverDurationAndDistanceList.get(0);
+                    int disInKilo = Double.valueOf(mDriverDurationAndDistance.getDistanceValue()).intValue()/1000;
+                    distanceWithKilo = String.valueOf(disInKilo);
+                    mEstimateFare = String.valueOf(TawsiliPublicFunc.calculateFairEstimate(mCategoryValue, disInKilo,true));
+                }
+                createOrder();
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                createOrder();
+            }
+        });
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq);
+    }
+
 }
