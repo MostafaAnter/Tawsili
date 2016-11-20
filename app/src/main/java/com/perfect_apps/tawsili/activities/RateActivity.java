aeb78f8a622db0b8763e8c60309af5a40fd77453
mostafa_anter <ga30000@gmail.com>
2016-11-20 19:12:58 +0200

    stable version

---
+++ app/src/main/java/com/perfect_apps/tawsili/activities/RateActivity.java
@@ -0,0 +1,196 @@
+package com.perfect_apps.tawsili.activities;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.widget.TextView;
+
+import com.akexorcist.localizationactivity.LocalizationActivity;
+import com.perfect_apps.tawsili.R;
+import com.perfect_apps.tawsili.store.TawsiliPrefStore;
+import com.perfect_apps.tawsili.utils.Constants;
+import com.perfect_apps.tawsili.utils.CustomTypefaceSpan;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RateActivity extends LocalizationActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.nav_view)NavigationView navigationView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate);
+
+        ButterKnife.bind(this);
+        setToolbar();
+        changeFontOfText();
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        changeFontOfNavigation();
+    }
+
+    //change font of drawer
+    private void changeFontOfNavigation(){
+        Menu m = navigationView.getMenu();
+        for (int i=0;i<m.size();i++) {
+            MenuItem mi = m.getItem(i);
+
+            //for aapplying a font to subMenu ...
+            SubMenu subMenu = mi.getSubMenu();
+            if (subMenu!=null && subMenu.size() >0 ) {
+                for (int j=0; j <subMenu.size();j++) {
+                    MenuItem subMenuItem = subMenu.getItem(j);
+                    applyFontToMenuItem(subMenuItem);
+                }
+            }
+
+            //the method we have create in activity
+            applyFontToMenuItem(mi);
+        }
+    }
+
+    private void applyFontToMenuItem(MenuItem mi) {
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/normal.ttf");
+        SpannableString mNewTitle = new SpannableString(mi.getTitle());
+        mNewTitle.setSpan(new CustomTypefaceSpan("" , font), 0 , mNewTitle.length(),  Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        mi.setTitle(mNewTitle);
+    }
+
+    private void changeFontOfText(){
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/normal.ttf");
+        Typeface fontBold = Typeface.createFromAsset(getAssets(), "fonts/bold.ttf");
+
+    }
+
+
+
+    private void setToolbar() {
+        setSupportActionBar(toolbar);
+        /*
+        * hide title
+        * */
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        //toolbar.setNavigationIcon(R.drawable.ic_toolbar);
+        toolbar.setTitle("");
+        toolbar.setSubtitle("");
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/bold.ttf");
+        TextView tv = (TextView) toolbar.findViewById(R.id.toolbar_title);
+        tv.setTypeface(font);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.my_rides_history) {
+            startActivity(new Intent(this, MyRidesActivity.class));
+
+        } else if (id == R.id.my_scheduled_rides) {
+            startActivity(new Intent(this, MyScheduleOrdersActivity.class));
+
+        } else if (id == R.id.invite_friends) {
+            startActivity(new Intent(this, InviteFriendActivity.class));
+
+        } else if (id == R.id.settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+        }else if (id == R.id.english_speaking){
+            showSingleChoiceListDrivereLangaugeAlertDialog();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    // change driver language
+    private String mCheckedDriverLanguageItem;
+
+    public void showSingleChoiceListDrivereLangaugeAlertDialog() {
+        final String[] list = new String[]{getString(R.string.language_arabic), getString(R.string.language_en)};
+        int checkedItemIndex;
+
+        switch (getDriverLanguage()) {
+            case "1":
+                checkedItemIndex = 1;
+                break;
+            default:
+                checkedItemIndex = 0;
+
+        }
+        mCheckedDriverLanguageItem = list[checkedItemIndex];
+
+        new AlertDialog.Builder(this)
+                .setTitle(getString(R.string.language))
+                .setSingleChoiceItems(list,
+                        checkedItemIndex,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mCheckedDriverLanguageItem = list[which];
+                                if (which == 0) {
+                                    setDriverLanguage("ar");
+                                    dialog.dismiss();
+                                } else if (which == 1) {
+                                    setDriverLanguage("en");
+                                    dialog.dismiss();
+                                }
+                            }
+                        })
+                .show();
+    }
+
+    private void setDriverLanguage(String langauge){
+        switch (langauge){
+            case "en":
+                new TawsiliPrefStore(this).addPreference(Constants.PREFERENCE_DRIVER_LANGUAGE, 2);
+                break;
+            case "ar":
+                new TawsiliPrefStore(this).addPreference(Constants.PREFERENCE_DRIVER_LANGUAGE, 1);
+                break;
+        }
+
+    }
+
+    private String getDriverLanguage(){
+        return String.valueOf(new TawsiliPrefStore(this)
+                .getIntPreferenceValue(Constants.PREFERENCE_DRIVER_LANGUAGE));
+    }
+}
